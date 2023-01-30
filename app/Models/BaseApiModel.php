ec4cd1cc2d44b2e712564e6b7e3cd25432d1c2f7
Pureshko <kotleta552@gmail.com>
2023-01-30 06:16:46 +0600

    group achievement

---
+++ app/Models/BaseApiModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\BaseApiFilesModel;
+
+abstract class BaseApiModel extends Model
+{
+    use HasFactory;
+    abstract protected function getList($token);
+    abstract protected function getListByUserId($id);
+    abstract protected function getById($id);
+    public function create($data){
+        return $this->insertGetId($data);
+    }
+    public function createFiles($id,$data){
+        if($data['photo1']){
+            $this->BaseApiFilesModel()->createFile($data['photo1'],$id);
+            if($data['photo2']){
+                $this->BaseApiFilesModel()->createtFile($data['photo2'],$id);
+                if($data['photo3']){
+                    $this->BaseApiFilesModel()->createtFile($data['photo3'],$id);
+                }else{
+                    return true;
+                }
+            }else{
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+    public function BaseApiFilesModel(){
+        return new BaseApiFilesModel();
+    }
+}
